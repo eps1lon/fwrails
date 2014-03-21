1b7110316a0f1653169ba27d06c8d4a06d0ff253
Benu <silbermann.sebastian@gmail.com>
2014-03-21 14:05:55 +0100

    environment normalization

--- lib/php/rails_const.php
+++ lib/php/rails_const.php
@@ -3,7 +3,6 @@
 define('RAILS_ROOT', realpath('../../'));
 
 $htaccess = file_get_contents(RAILS_ROOT . '/public/.htaccess');
-define('RAILS_ENV', 'production');
 
 if (!defined('RAILS_ENV')) {
     if (preg_match("/RailsEnv\s+(\w+)/i", $htaccess, $matches)) {
