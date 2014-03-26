0c9d85f763a4227610f3532c9afe8dec526206a5
Benu <silbermann.sebastian@gmail.com>
2014-03-26 12:42:43 +0100

    fixed namechanges attr_accessors in users#show

--- app/models/users_name_change.rb
+++ app/models/users_name_change.rb
@@ -1,12 +1,17 @@
 class UsersNameChange < ActiveRecord::Base
   self.primary_keys = :user_id, :world_id, :created_at
   
-  alias_attribute :old, :name_old
-  alias_attribute :new, :name_new
-  
   belongs_to :world
   belongs_to :user, :foreign_key => [:user_id, :world_id] 
- 
+  
+  def old
+    {:name => self.name_old}
+  end
+  
+  def new
+    {:name => self.name_new}
+  end
+  
   # belongs_to :user, :conditions => {:self => {:deleted => false}}
   alias_method :__user, :user
   def user
