fd27fe582bd8a9533c6327d2f22e8844289c4168
Benu <silbermann.sebastian@gmail.com>
2014-04-03 10:49:26 +0200

    added various urls to clans#show and users#show

--- app/models/concerns/clan_urls.rb
+++ app/models/concerns/clan_urls.rb
@@ -4,4 +4,8 @@ module ClanUrls
   def profile_url
     "#{world.url}internal/fight.php?action=watchclan&act_clan_id=#{clan_id}"
   end
+  
+  def rank_url
+    "#{world.url}internal/fight.php?action=clanrang&act_clan_id=#{clan_id}"
+  end
 end
