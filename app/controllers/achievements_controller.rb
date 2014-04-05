dd6957b024c5942c3ccfb250198af9b0a057960b
Benu <silbermann.sebastian@gmail.com>
2014-04-05 20:53:21 +0200

    secondard order now progress in achievements#rank

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -39,6 +39,8 @@ class AchievementsController < ApplicationController
       @order = ['count']
     end
     
+    @order << 'progress'
+    
     # groups of achievements which can be display
     @achiev_groups = Achievement.base_stage.order(:name)
     
