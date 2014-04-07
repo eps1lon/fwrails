eab1171fefe492d76e1da487daaec59ccc206354
Benu <silbermann.sebastian@gmail.com>
2014-04-07 11:26:18 +0200

    removed public? since rails automagical creates these methods for boolean attributes used Pathname instead of strings updated_at returns max mtime of children added children method that returns [nil] if no children

--- app/models/dump.rb
+++ app/models/dump.rb
@@ -1,37 +1,38 @@
 class Dump < ActiveRecord::Base
+  scope :public, -> { where(public: true) }
+  
+  def children
+    return system_path.children(false) if subs?
+    [nil]
+  end
+  
   def files
-    files = [nil]
-    files = Dir.open(system_path).each.reject { |dir| %w{. ..}.include?(dir) } if self.subs?
-
-    files.map { |f| web_path(f) }
+    children.map { |f| web_path(f) }
   end
   
   def updated_at
-    File.stat(system_path).mtime
+    return system_path.mtime unless subs?
+    children.map { |f| system_path.join(f) }.map(&:mtime).max
   end
   
-  def public?
-    !self['public'].zero?
+  def path
+    Pathname.new(super)
   end
   
   def subs?
-    File.directory?(system_path)
+    system_path.directory?
   end
   
   def web_path(file = nil)
     unless file.nil?
-      return File.join(self.path, file)
+      return path.join(file)
     end
-    self.path
-  end
-  
-  def self.public
-    where(:public => true)
+    path
   end
   
   private   
   
   def system_path
-    Rails.root.join("public", "dumps", self.path)
+    Rails.root.join("public", "dumps", path)
   end
 end
