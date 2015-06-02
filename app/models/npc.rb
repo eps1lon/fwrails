7f9a3f0010ba26df7491991af53fb597cfa4e172
benu <silbermann.sebastian@gmail.com>
2015-06-02 18:01:07 +0200

    moved conditions submodule to scope

--- app/models/npc.rb
+++ app/models/npc.rb
@@ -1,7 +1,17 @@
 class Npc < ActiveRecord::Base
+  include InvertScope
+  
   belongs_to :place, foreign_key: [:pos_x, :pos_y],
                      primary_key: [:pos_x, :pos_y]
-  enum unique_npc: {npc: 1, unique_npc: 2, group_npc: 3, unknown: 4, passiv: 5, resistance: 6, superresistance: 7}
+  enum unique_npc: {
+    npc: 1, 
+    unique_npc: 2, 
+    group_npc: 3, 
+    unknown: 4, 
+    passiv: 5, 
+    resistance: 6, 
+    superresistance: 7
+  }
   
   has_many :drops, -> { where(items_npcs: {action: ItemsNpc.actions[:kill]}) }, 
                    class_name: "ItemsNpc"
@@ -9,51 +19,33 @@ class Npc < ActiveRecord::Base
   scope :in_range, ->(x, y, r) { where(pos_x: (x - r)..(x + r), pos_y: (y - r)..(y + r)) }
   scope :in_ressurect_range, ->(x, y) { in_rage(x, y, 2) }
   
-  scope :slmania_list, -> { where.not(Npc::Conditions.bloodnpcs[:all]).
-                            where.not(Npc::Conditions.shadowcreatures[:all]).
-                            group(:name).order(:name) }
+  scope :bloodnpcs, -> { where(unique_npc: [Npc.unique_npcs[:unique_npc], Npc.unique_npcs[:group_npc]]).
+                         where(table[:id].lt(0)).
+                         where(Arel::Nodes::NamedFunction.new("LOCATE", ['-', table[:name]]).gt(0)) }
   
-  scope :persistent_npcs, -> { where.not(Npc::Conditions.bloodnpcs[:all]).
-                               where.not(Npc::Conditions.shadowcreatures[:all]).
-                               where.not(Npc::Conditions.dancer).
-                               where.not(Npc::Conditions.servant).
+  scope :dancers, -> { where(table[:id].lt(0)).
+                       where(table[:name].matches("Tänzerin von %")) }
+  
+  scope :killable, -> { where.not(unique_npc: Npc.unique_npcs[:passiv]) }
+  
+  scope :servants, -> { where(table[:id].lt(0)).
+                        where(table[:name].matches("Diener von %")) }
+  
+  scope :shadowcreatures, -> { where(table[:id].lt(0)).
+                               where(table[:name].matches("Schattenkreatur %"))}
+  
+  scope :slmania_npcs, -> { persistent_npcs.killable.
+                            group(:name).reorder(:name) }
+  
+  scope :persistent_npcs, -> { invert(:bloodnpcs).
+                               invert(:shadowcreatures).
+                               invert(:dancers).
+                               invert(:servants).
                                order(:id)
                                }
   
-  module Conditions
-    def self.table
-      Npc.arel_table
-    end
-    
-    def self.bloodnpcs
-      npcs = Npc.arel_table
-      {
-        :all => npcs[:id].lt(0).
-                and(npcs[:unique_npc].gt(1)).
-                and(Arel::Nodes::NamedFunction.new("LOCATE", ['-', npcs[:name]]).gt(0))   
-      }
-    end
-    
-    def self.shadowcreatures
-      npcs = Npc.arel_table
-      types = {all: npcs[:id].lt(0).and(npcs[:name].matches("Schattenkreatur %"))}
-      Npc.group(:unique_npc).each do |npc|
-        types[npc.unique_npc] = types[:all].and(npcs[:unique_npc].eq(npc.unique_npc))
-      end
-      types
-    end
-    
-    def self.dancer
-      table[:id].lt(0).and(table[:name].matches("Tänzerin von %"))
-    end
-    
-    def self.servant
-      table[:id].lt(0).and(table[:name].matches("Diener von %"))
-    end
-    
-    def self.npc(name)
-      {name => Npc.arel_table[:name].eq(name)}
-    end
+  def self.table
+    Npc.arel_table
   end
   
   FLAGS = [:aggressive]
